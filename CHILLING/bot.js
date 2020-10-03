13957fd6e789a8f6b992df007bba71abcf19af08
Gorott <67421407+Gorott@users.noreply.github.com>
2020-10-03 22:40:38 +0200

    ffe
    feefe

---
+++ CHILLING/bot.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const discord = require('discord.js');
+const client = new discord.Client();
+const PREFIX = process.env.PREFIX;
+client.login(process.env.BOT_TOKEN);
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in`);
+});
+const isValidCommand = (message, cmdName) => message.content.toLowerCase().startsWith(PREFIX + cmdName);
+const rollDice = () => Math.floor(Math.random() * 6) + 1;
+client.on('message', function(message) {
+    if (message.content === "$loop") {
+    var interval = setInterval (function () {
+        // use the message's channel (TextChannel) to send a new message
+        message.channel.send("<@544805979179712522>")
+    },1000); 
+}
+});
