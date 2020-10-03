1e7dea483b67f58a6d938713f31ce607e84ba5bf
jfvdcsplddkddcpdvdcddk <72320407+jfvdcsplddkddcpdvdcddk@users.noreply.github.com>
2020-10-03 22:41:32 +0200

    Update bot.js

--- CHILLING/bot.js
+++ CHILLING/bot.js
@@ -1,3 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+
 require('dotenv').config();
 const discord = require('discord.js');
 const client = new discord.Client();
